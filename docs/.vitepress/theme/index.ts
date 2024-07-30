4dcf1406d25efd284dcc519305840d9aa1080a2b
waterbang <409071692@qq.com>
2024-07-30 17:30:20 +0800

    :art: 让文档跟app样式同频

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,11 +3,19 @@ import TwoslashFloatingVue from "@shikijs/vitepress-twoslash/client";
 import "@shikijs/vitepress-twoslash/style.css";
 import "uno.css";
 import type { EnhanceAppContext } from "vitepress";
-import Theme from "vitepress/theme";
+import { default as DefaultTheme, default as Theme } from "vitepress/theme";
+import { h } from "vue";
+import IndexBackground from "../components/IndexBackground.vue";
+import "./style/var.css";
 
 export default {
   extends: Theme,
   enhanceApp({ app }: EnhanceAppContext) {
     app.use(TwoslashFloatingVue);
+  },
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      "home-hero-before": () => h(IndexBackground),
+    });
   },
 };
